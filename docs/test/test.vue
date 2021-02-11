bf477aa909f7a37753c903bff90b39e14f9c900c
imuradevelopment <imuradevelopmentauth@gmail.com>
2021-02-12 00:57:38 +0900

    記事一覧

---
+++ docs/test/test.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>-------- docs --------</h2>
+    <section>
+      <article v-for="doc in docs">
+        <h3>{{doc.title}}</h3>
+        <pre>{{JSON.stringify(doc.frontmatter, null, 2)}}</pre>
+      </article>
+    </section>
+    <h2>-------- blogs --------</h2>
+    <section>
+      <article v-for="blog in blogs">
+        <h3>{{blog.title}}</h3>
+        <pre>{{JSON.stringify(blog.frontmatter, null, 2)}}</pre>
+      </article>
+    </section>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  computed: {
+    docs() {
+      return this.$site.pages.filter(d => d.path.startsWith('/docs/'))
+    },
+    blogs() {
+      return this.$site.pages.filter(d => d.path.startsWith("/blogs/"))
+    }
+  },
+}
+</script>
+
+<style module>
+
+</style>
